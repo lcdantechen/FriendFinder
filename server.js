c04d4957f9432bbe5c962f1796e22f9aa89bb2d3
Dante <lcdantechen@gmail.com>
2016-07-31 13:42:47 -0400

    fuction works

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ var characters = [
 		name:"car",
     photo:"http://oi47.tinypic.com/6t1esg.jpg",
     scores:[
-     5,
      1,
-     4
+     5,
+     1
    ]	
 	},
 
@@ -37,8 +37,8 @@ var characters = [
 		name:"Brad Pitt",
     photo:"https://s-media-cache-ak0.pinimg.com/236x/42/29/17/4229178045bad6d6f5f47efc6ec8b0c9.jpg",
     scores:[
-     5,
-     1,
+     2,
+     3,
      4
    ]
 	}
@@ -62,14 +62,40 @@ app.post('/api/friends', function(req, res){
 		userScore.push(eachScore);
 	}
 	console.log(userScore);
-	var userInventory = 
-	/*var difference;
-	difference = */
+	var difference = 0;
+	var absoluteValue = 0;
+	var absoluteValueDifference = [];
+	
+  
+  for (var x=0; x<characters.length; x++) {
+
+		  for (var i=0; i<userScore.length; i++) {
+		  	difference = difference + (Math.abs(characters[x].scores[i] - userScore[i]));
+
+		  }
+		  absoluteValue = difference;
+		  absoluteValueDifference.push(absoluteValue);
+		  difference = 0;
+		  console.log(absoluteValue);
+	 }
+	 console.log(absoluteValueDifference);
+	 var min = absoluteValueDifference[0];
+	 var minIndex = 0;
+   
+   for (var j=1; j<absoluteValueDifference.length; j++) {
+   	  if (absoluteValueDifference[j] < min) {
+   	  	  minIndex=j;
+   	  	  console.log(minIndex);
+   	  	  console.log("inside for loop")
+   	  	  min = absoluteValueDifference[j];
+   	  }
+   }
+	
 
 
-	characters.push(newcharacter);
-
-	res.json(newcharacter);
+	/*characters.push(newcharacter);*/
+  console.log(minIndex);
+	res.json(characters[minIndex]);
 })
 
 app.listen(PORT, function(){
